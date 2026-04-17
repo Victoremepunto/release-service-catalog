2525bfc6d44d82037d320e5a2399775eb2f2a103
Lukas Slebodnik <lslebodn@redhat.com>
2026-04-17 20:45:23 +0200

    Change expected format of secrets
    Previous approach was very error prone for changes
    by release engineers.
    
    One can easily to a mistake in comma separated base64 encoded jsons

--- tasks/managed/marketplacesvm-push-disk-images/tests/pre-apply-task-hook.sh
+++ tasks/managed/marketplacesvm-push-disk-images/tests/pre-apply-task-hook.sh
@@ -8,4 +8,30 @@ yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[
 
 # Create a dummy secret (and delete it first if it exists)
 kubectl delete secret marketplacesvm-test-secret --ignore-not-found
-kubectl create secret generic marketplacesvm-test-secret --from-literal=key=eyJ0ZXN0Ijoic2VjcmV0In0K
+kubectl create secret generic marketplacesvm-test-secret \
+  --from-literal=aws-na.json='{
+    "marketplace_account": "aws-na",
+    "auth": {
+        "AWS_IMAGE_ACCESS_KEY": "AK-TEST-NA",
+        "AWS_IMAGE_SECRET_ACCESS": "sa-test-na-secret",
+        "AWS_MARKETPLACE_ACCESS_KEY": "AK-TEST-NA-MKT",
+        "AWS_MARKETPLACE_SECRET_ACCESS": "sa-test-na-mkt-secret",
+        "AWS_ACCESS_ROLE_ARN": "arn:aws:iam::555555555555:role/AWSMarketplaceScanning",
+        "AWS_GROUPS": [],
+        "AWS_SNAPSHOT_ACCOUNTS": [],
+        "AWS_REGION": "us-east-1"
+    }
+}' \
+  --from-literal=aws-emea.json='{
+    "marketplace_account": "aws-emea",
+    "auth": {
+        "AWS_IMAGE_ACCESS_KEY": "AK-TEST-EMEA",
+        "AWS_IMAGE_SECRET_ACCESS": "sa-test-emea-secret",
+        "AWS_MARKETPLACE_ACCESS_KEY": "AK-TEST-EMEA-MKT",
+        "AWS_MARKETPLACE_SECRET_ACCESS": "sa-test-emea-mkt-secret",
+        "AWS_ACCESS_ROLE_ARN": "arn:aws:iam::555555555555:role/AWSMarketplaceScanning",
+        "AWS_GROUPS": [],
+        "AWS_SNAPSHOT_ACCOUNTS": [],
+        "AWS_REGION": "eu-central-1"
+    }
+}'
